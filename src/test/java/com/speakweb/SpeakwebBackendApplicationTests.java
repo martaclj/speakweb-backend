19f236ef4d1a7b14260b94d1d56576a771051230
martaclj <104948192+martaclj@users.noreply.github.com>
2025-11-01 14:47:47 +0100

    Initial commit- backend

---
+++ src/test/java/com/speakweb/SpeakwebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.speakweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeakwebBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
