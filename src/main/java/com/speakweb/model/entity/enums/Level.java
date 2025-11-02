c786cdba8bf2f3fec500b9951c3a542b7b451d9c
martaclj <marlarrad@gmail.com>
2025-11-02 21:13:33 +0100

    add User, Lang and UserLang entities

---
+++ src/main/java/com/speakweb/model/entity/enums/Level.java
@@ -0,0 +1,11 @@
+package com.speakweb.model.entity.enums;
+
+public enum Level {
+	
+	A1,
+	A2,
+	B1,
+	B2,
+	C1,
+	C2
+}
---
+++ src/main/java/com/speakweb/model/entity/enums/UserLangType.java
@@ -0,0 +1,6 @@
+package com.speakweb.model.entity.enums;
+
+public enum UserLangType {
+	NATIVE,
+	LEARNER
+}
