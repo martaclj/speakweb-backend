d7a5130dd0f224dbca1e2d7ade97a845a6d8faee
martaclj <marlarrad@gmail.com>
2025-12-03 18:18:29 +0100

    LanguageController v1

---
+++ src/main/java/com/speakweb/restcontroller/LanguageController.java
@@ -0,0 +1,36 @@
+package com.speakweb.restcontroller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.speakweb.model.entity.Language;
+import com.speakweb.model.service.LanguageService;
+
+@RestController
+@RequestMapping("/api/languages")
+public class LanguageController {
+	
+	@Autowired
+	private LanguageService languageService;
+	
+	@GetMapping
+	public ResponseEntity<List<Language>> getAllLanguages() {
+		
+		return ResponseEntity.ok(languageService.getAllLanguages());
+	}
+	
+	@PostMapping
+	public ResponseEntity<Language> createLanguage(@RequestBody Language language) {
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(languageService.save(language));
+	}
+
+}
