460947113975ac1c77957de51bf1b099e5155cd3
martaclj <marlarrad@gmail.com>
2025-12-05 19:12:08 +0100

    GroupMemberController v1

--- src/main/java/com/speakweb/model/service/GroupMemberService.java
+++ src/main/java/com/speakweb/model/service/GroupMemberService.java
@@ -1,5 +1,9 @@
 package com.speakweb.model.service;
 
+import java.util.List;
+
+import com.speakweb.model.entity.GroupMember;
+
 public interface GroupMemberService {
 
 	boolean isAdmin(int userId); // el admin puede crear eventos siempre
@@ -7,4 +11,7 @@ public interface GroupMemberService {
 	boolean isExpertInGroup(int userId, int groupId); // es experto?
 	boolean canCreateEvent(int userId, int groupId); // es admin o experto del grupo? -> puede abrir evento
 	
+	GroupMember joinGroup(String userEmail, int groupId); // userEmail viene del token de seguridad
+	List<GroupMember> getGroupMembers(int groupId); // miembros de un grupo
+	List<GroupMember> getMyGroups(String userEmail); // email del token
 }
