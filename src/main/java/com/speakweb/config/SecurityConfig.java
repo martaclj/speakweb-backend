cd0b4567c9639a340e3b82f5fa81aaab1850e961
martaclj <104948192+martaclj@users.noreply.github.com>
2026-03-13 22:33:42 +0100

    add HttpS to CORS

--- src/main/java/com/speakweb/config/SecurityConfig.java
+++ src/main/java/com/speakweb/config/SecurityConfig.java
@@ -72,7 +72,7 @@ public class SecurityConfig {
 	@Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200", "https://speakweb.martaclj.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
        // configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
